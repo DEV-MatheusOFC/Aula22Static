e4e1681a82392e5c7d3923729911610e258c7352
DEV-MatheusOFC <matheusnunesoliveira198@gmail.com>
2020-06-22 09:46:43 -0300

    Aula22Static Finalizada

---
+++ Conversor.cs
@@ -0,0 +1,24 @@
+namespace Aula_22___Static
+{
+    public static class Conversor
+    {
+       public static float CotacaoDolar = 5.29f;
+       public static float CotacaoEuro = 5.90f;
+
+        public static float RealParaDolar(float valor){
+        return valor / CotacaoDolar;
+        }
+
+        public static float DolarParaReal(float valor){
+        return valor * CotacaoDolar;
+        }
+
+        public static float RealParaEuro(float valor){
+        return valor / CotacaoDolar;
+        }
+
+        public static float EuroParaReal(float valor){
+        return valor * CotacaoEuro;
+        }
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula_22___Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Conversão de Valores em Dolar:");
+
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Conversor.CotacaoDolar = 5.29f;
+            Console.WriteLine( Conversor.DolarParaReal(52) );
+            Console.WriteLine( Conversor.RealParaDolar(52) );
+
+            Console.ResetColor();
+
+            //
+            System.Console.WriteLine("Conversão de Valores em Euro:");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Conversor.CotacaoEuro = 5.90f;
+            Console.WriteLine( Conversor.EuroParaReal(52) );
+            Console.WriteLine( Conversor.RealParaEuro(52) );
+
+            Console.ResetColor();
+        }
+    }
+}
